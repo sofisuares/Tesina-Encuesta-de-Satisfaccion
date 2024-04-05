9b1d4c69e4ffa06ccbcee00fdafe69faf4b453d9
sofisuares <166163938+sofisuares@users.noreply.github.com>
2024-04-05 15:34:22 -0300

    Muestra 5 segmentos 4 planes.R

--- Muestra 5 segmentos 4 planes.R
+++ Muestra 5 segmentos 4 planes.R
@@ -9,10 +9,10 @@
 ### Resumen:
 #Estimadores: proporción satisfechos, promotores y detractores (3 variables)
 #Dominios de estimación: 
-#DOM1= PLAN (Cerrado-Integral-Superior-Selecta)
-#DOM2= NIVEL USO (menor a 0.5, menor a 1.5, mayor a 1.5)
-#DOM3= REGION (AMBA-BSAS-PATAGONIA-LITORAL-NUCLEO-CENTRONOA)
-#DOM4= SEGMENTO (DTCAP-VOLDIR-VOLEMP-OBLDIR-OBLEMP)
+#DOM1= PLAN (A - B - C - D)
+#DOM2= NIVEL USO (Bajo - Medio - Alto)
+#DOM3= REGION (A - B - P - L - N - C)
+#DOM4= SEGMENTO (A - B - C - D - E )
 #DOM5= GLOBAL (Total)
 
 ### Carga de paquetes a usar
@@ -235,4 +235,4 @@ Muestra <- Marco1[Muestra1$ID_unit,]  #Para obtener el número de GF. De Marco1 
 ### Exportar muestra a excel
 #install.packages("openxlsx")
 library(openxlsx)
-write.xlsx(Muestra, "Muestra5SegmentosConCerrado.xlsx")
+write.xlsx(Muestra, "Muestra5Segmentos4Planes.xlsx")
